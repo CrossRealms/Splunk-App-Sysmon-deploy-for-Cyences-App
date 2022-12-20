a0f220f8f4eaa19683dc4fbca1178e2eda14a051
Mahir Chavda <mahir.chavda13@gmail.com>
2022-12-20 22:23:49 +0530

    minor log change

--- TA-sysmon-deploy-for-cyences/bin/update.bat
+++ TA-sysmon-deploy-for-cyences/bin/update.bat
@@ -3,6 +3,6 @@ ECHO OFF
 SET SPLUNKPATH=%~dp0
 
 >> C:\Windows\sysmon.log (
-fc C:\Windows\sysmonconfig.xml "%SPLUNKPATH%\sysmonconfig.xml" | Find "no differences" 1>nul && echo %DATE%-%TIME% No updates found && exit
-echo %DATE%-%TIME% Update found, installing && copy /z /y "%SPLUNKPATH%\sysmonconfig.xml" "C:\Windows\" && "C:\Windows\Sysmon.exe" -c C:\Windows\sysmonconfig.xml
+fc C:\Windows\sysmonconfig.xml "%SPLUNKPATH%\sysmonconfig.xml" | Find "no differences" 1>nul && echo %DATE%-%TIME% No config update found && exit
+echo %DATE%-%TIME% config update found, installing && copy /z /y "%SPLUNKPATH%\sysmonconfig.xml" "C:\Windows\" && "C:\Windows\Sysmon.exe" -c C:\Windows\sysmonconfig.xml
 )
